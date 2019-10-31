b71d38488def6a45dfc4f11d7315ea743bce2da3
ErickMora <erick_3069@hotmail.com>
2019-10-31 00:08:42 -0500

    async

--- 01-Javascript/06-sync-async.js
+++ 01-Javascript/06-sync-async.js
@@ -2,7 +2,7 @@
 //no 1: 5-3-2-4
 const fileSystem = require('fs');
 console.log(('1) Iniciando'));
-fileSystem.readFile(
+/*fileSystem.readFile(
     './05-callbacks.js',
     'utf8',
     //() => { //callback
@@ -82,3 +82,96 @@ function callbackLeerArchivo(error, datos) {
 
 }
 
+*/
+
+
+fileSystem.readFile(
+    './05-callbacks.js',
+    'utf8',
+    (error, datos) => {
+        if (error) {
+            console.error({
+                mensaje: 'Error leyendo archivo',
+                // error: error,
+                error,
+            })
+        } else {
+            console.log('Datos: ', datos);
+            fileSystem.readFile(
+                './04-funciones.js',
+                'utf8',
+                (error, datos) => {
+                    if (error) {
+                        console.error({
+                            mensaje: 'Error leyendo archivo',
+                            // error: error,
+                            error,
+                        })
+                    } else {
+                        console.log('Datos: ', datos);
+                        fileSystem.readFile(
+                            './03-operadores.js',
+                            'utf8',
+                            (error, datos) => {
+                                if (error) {
+                                    console.error({
+                                        mensaje: 'Error leyendo archivo',
+                                        // error: error,
+                                        error,
+                                    })
+                                } else {
+                                    console.log('Datos: ', datos);
+                                    fileSystem.readFile(
+                                        './02-objetos.js',
+                                        'utf8',
+                                        (error, datos) => {
+                                            if (error) {
+                                                console.error({
+                                                    mensaje: 'Error leyendo archivo',
+                                                    // error: error,
+                                                    error,
+                                                })
+                                            } else {
+                                                console.log('Datos: ', datos);
+                                                fileSystem.readFile(
+                                                    './01-variables.js',
+                                                    'utf8',
+                                                    (error, datos) => {
+                                                        if (error) {
+                                                            console.error({
+                                                                mensaje: 'Error leyendo archivo',
+                                                                // error: error,
+                                                                error,
+                                                            })
+                                                        } else {
+                                                            console.log('Datos: ', datos);
+                                                        }
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    }
+);
+
+function callbackLeerArchivo(error, datos) {
+    if (error) {
+        consol.error({
+            mensaje: 'Error leyendo archivo',
+            // error: error,
+            error,
+        })
+    } else {
+        console.log('Datos: ', datos);
+    }
+}
+
+
+console.log('2) Finalizando');
